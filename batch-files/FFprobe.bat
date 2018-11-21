451aec40dd2ee6122177ecec84d2aa944955316d
Olivier Korach <olivier.korach@gmail.com>
2018-11-21 23:28:07 +0100

    new use cases

---
+++ batch-files/FFprobe.bat
@@ -0,0 +1,2 @@
+rem E:\Tools\ffmpeg-3.4-win64-static\bin\ffprobe -i %1 -show_entries -v quiet -of csv="p=0" >%1.txt
+E:\Tools\ffmpeg-3.4-win64-static\bin\ffprobe -v error -show_format -show_streams %1 >%1.txt
