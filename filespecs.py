a0cf4b9e631aa7d2b1580378ee7b29fa0f9284ff
Olivier Korach <olivier.korach@gmail.com>
2018-12-01 18:19:43 +0100

    Externalized file specs + add mp3 tags

--- filespecs.py
+++ filespecs.py
@@ -20,56 +20,37 @@ def parse_args():
     return args
 
 args = parse_args()
-specs = videotools.videofile.get_file_specs(args.inputfile)
-file_type = specs['format']['format_name']
-myspecs = dict()
-myspecs['file_name'] = specs['format']['filename']
-myspecs['file_size'] = specs['format']['size']
-if file_type == 'image2':
-    myspecs['type'] = 'image'
-elif file_type == 'mp3':
-    myspecs['type'] = 'audio'
-    myspecs['format'] = specs['streams'][0]['codec_name']
-elif re.search(r'mp4', file_type) is not None:
-    myspecs['type'] = 'video'
-    myspecs['format'] = 'mp4'
-
-for stream in specs['streams']:
-    try:
-        if myspecs['type'] == 'image':
-            myspecs['image_codec'] = stream['codec_name']
-            myspecs['width'] = stream['width']
-            myspecs['height'] = stream['height']
-            myspecs['format'] = stream['codec_name']
-        elif myspecs['type'] == 'video' and stream['codec_type'] == 'video':
-            myspecs['type'] = 'video'
-            myspecs['video_codec'] = stream['codec_name']
-            myspecs['video_bit_rate'] = stream['bit_rate']
-            myspecs['width'] = stream['width']
-            myspecs['height'] = stream['height']
-            myspecs['duration'] = stream['duration']
-            myspecs['duration_hms'] = videotools.videofile.to_hms_str(stream['duration'])
-            myspecs['aspect_ratio'] = stream['display_aspect_ratio']
-            myspecs['frame_rate'] = stream['r_frame_rate']
-        elif (myspecs['type'] == 'audio' or myspecs['type'] == 'video') and stream['codec_type'] == 'audio':
-            myspecs['audio_codec'] = stream['codec_name']
-            myspecs['sample_rate'] = stream['sample_rate']
-            myspecs['duration'] = stream['duration']
-            myspecs['duration_hms'] = videotools.videofile.to_hms_str(stream['duration'])
-            myspecs['audio_bit_rate'] = stream['bit_rate']
-    except KeyError as e:
-        print("Stream %s has no key %s" % (str(stream), e.args[0]))
-nb_streams = specs['format']['nb_streams']
-
-if (args.format is "txt" ):
-    for param in myspecs:
-        print("%-20s : %s" % (param, str(myspecs[param])))
+if os.path.isdir(args.inputfile):
+    filelist = videotools.videofile.filelist(args.inputfile)
 else:
-    # CSV format
+    filelist = [ args.inputfile ]
+
+is_first = True
+
+props = ['filename', 'filesize', 'type', 'format', 'width', 'height', 'video_codec', 'audio_codec', \
+    'audio_bitrate', 'video_bitrate', 'duration', 'duration_hms', 'sample_rate', 'fps', 'aspect_ratio', \
+    'author', 'title', 'album', 'year', 'track', 'genre']
+
+if args.format is not 'txt':
     print("# ")
-    for param in myspecs:
-        print("%s; " % param, end='')
+    for prop in props:
+        print("%s;" % prop, end='')
     print('')
-    for param in myspecs:
-        print("%s; " % str(myspecs[param]), end='')
-    print('')
+
+for file in filelist:
+    if not videotools.videofile.is_media_file(file):
+        continue
+    try:
+        myspecs = videotools.videofile.get_file_specs(file)
+        for prop in props:
+            if args.format is "txt":
+                print("%-20s : %s" % (prop, str(myspecs[prop])))
+            else:
+                # CSV format
+                try:
+                    print("%s;" % str(myspecs[prop]), end='')
+                except KeyError:
+                    print("%s;" % '', end='')
+        print('')
+    except videotools.videofile.FileTypeError as e:
+        print ('ERROR: File %s type error' % file)
