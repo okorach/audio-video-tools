72eab74ab062ec7a8efcd6fdb167c5fa10fcc0f4
Olivier Korach <olivier.korach@gmail.com>
2018-12-01 15:39:14 +0100

    Add CSV format support

--- filespecs.py
+++ filespecs.py
@@ -8,7 +8,7 @@ import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser(
-            description='Python wrapper for ffmpeg.')
+            description='Get Audio/Video/Image file specifications')
     parser.add_argument('-i', '--inputfile', required=True,
                            help='Input file'
                         )
@@ -35,29 +35,41 @@ elif re.search(r'mp4', file_type) is not None:
     myspecs['format'] = 'mp4'
 
 for stream in specs['streams']:
-    if file_type == 'image2':
-        myspecs['image_codec'] = stream['codec_name']
-        myspecs['width'] = stream['width']
-        myspecs['height'] = stream['height']
-        myspecs['format'] = stream['codec_name']
-        myspecs['codec'] = stream['codec_name']
-    elif stream['codec_type'] == 'video':
-        myspecs['type'] = 'video'
-        myspecs['video_codec'] = stream['codec_name']
-        myspecs['video_bit_rate'] = stream['bit_rate']
-        myspecs['width'] = stream['width']
-        myspecs['height'] = stream['height']
-        myspecs['duration'] = stream['duration']
-        myspecs['duration_hms'] = videotools.videofile.to_hms_str(stream['duration'])
-        myspecs['aspect_ratio'] = stream['display_aspect_ratio']
-        myspecs['frame_rate'] = stream['r_frame_rate']
-    elif stream['codec_type'] == 'audio':
-        myspecs['audio_codec'] = stream['codec_name']
-        myspecs['sample_rate'] = stream['sample_rate']
-        myspecs['duration'] = stream['duration']
-        myspecs['duration_hms'] = videotools.videofile.to_hms_str(stream['duration'])
-        myspecs['audio_bit_rate'] = stream['bit_rate']
+    try:
+        if myspecs['type'] == 'image':
+            myspecs['image_codec'] = stream['codec_name']
+            myspecs['width'] = stream['width']
+            myspecs['height'] = stream['height']
+            myspecs['format'] = stream['codec_name']
+        elif myspecs['type'] == 'video' and stream['codec_type'] == 'video':
+            myspecs['type'] = 'video'
+            myspecs['video_codec'] = stream['codec_name']
+            myspecs['video_bit_rate'] = stream['bit_rate']
+            myspecs['width'] = stream['width']
+            myspecs['height'] = stream['height']
+            myspecs['duration'] = stream['duration']
+            myspecs['duration_hms'] = videotools.videofile.to_hms_str(stream['duration'])
+            myspecs['aspect_ratio'] = stream['display_aspect_ratio']
+            myspecs['frame_rate'] = stream['r_frame_rate']
+        elif (myspecs['type'] == 'audio' or myspecs['type'] == 'video') and stream['codec_type'] == 'audio':
+            myspecs['audio_codec'] = stream['codec_name']
+            myspecs['sample_rate'] = stream['sample_rate']
+            myspecs['duration'] = stream['duration']
+            myspecs['duration_hms'] = videotools.videofile.to_hms_str(stream['duration'])
+            myspecs['audio_bit_rate'] = stream['bit_rate']
+    except KeyError as e:
+        print("Stream %s has no key %s" % (str(stream), e.args[0]))
 nb_streams = specs['format']['nb_streams']
 
-for param in myspecs:
-    print("%-20s : %s" % (param, str(myspecs[param])))
+if (args.format is "txt" ):
+    for param in myspecs:
+        print("%-20s : %s" % (param, str(myspecs[param])))
+else:
+    # CSV format
+    print("# ")
+    for param in myspecs:
+        print("%s; " % param, end='')
+    print('')
+    for param in myspecs:
+        print("%s; " % str(myspecs[param]), end='')
+    print('')
