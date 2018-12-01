dde5b913f23580c8df133edca86791d8e74cb65b
Olivier Korach <olivier.korach@gmail.com>
2018-12-01 14:52:46 +0100

    Python style printf

--- album_art.py
+++ album_art.py
@@ -12,9 +12,9 @@ if album_art is None:
     print('Album Art image file not found')
     exit(1)
 else:
-    print('Encoding Album Art image file ' + album_art)
+    print('Encoding Album Art image file %s' % album_art)
 
 for file in sys.argv:
     if videotools.videofile.is_audio_file(file):
-        print('Encoding audio file ' + file)
+        print('Encoding audio file %s' % file)
         videotools.videofile.encode_album_art(file, album_art)
