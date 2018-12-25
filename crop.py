9675d2ff34c75a5c7c4b8991f5184fd4d3afa46f
Olivier Korach <olivier.korach@gmail.com>
2018-12-25 16:05:00 +0100

    add capability to crop

---
+++ crop.py
@@ -0,0 +1,23 @@
+#!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3
+
+from videotools.videofile import crop
+from videotools.filetools import debug, set_debug_level
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Apply deshake filter')
+    parser.add_argument('-i', '--inputfile', required=True, help='Input File or Directory to encode')
+    parser.add_argument('-o', '--outputfile', required=False, help='Output file to generate')
+    parser.add_argument('-g', '--debug', required=False, help='Debug level')
+    parser.add_argument('-b', '--box', required=True, help='Video box')
+    parser.add_argument('-t', '--top', required=True, help='Video top origin')
+    parser.add_argument('-l', '--left', required=True, help='Video left origin')
+    args = parser.parse_args()
+    return args
+
+args = parse_args()
+set_debug_level(args.debug)
+
+width, height = args.box.split("x")
+outputfile = crop(args.inputfile, int(width), int(height), int(args.top), int(args.left), args.outputfile)
+debug(1, 'Generated %s' % outputfile)
