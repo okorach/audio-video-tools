b18ae92bf44df13b2ae575e0e4fae11b39355a04
Olivier Korach <olivier.korach@gmail.com>
2018-12-30 18:22:18 +0100

    Pylint pass + added new stuff from videofile

--- mediatools/utilities.py
+++ mediatools/utilities.py
@@ -1,11 +1,12 @@
 #!python3
- 
-import os
-import re
-import jprops
-import platform
 
-debug_level = 0
+import os
+import sys
+import re
+import platform
+import jprops
+
+DEBUG_LEVEL = 0
 
 FFMPEG_FORMAT_OPTION = 'f'
 FFMPEG_SIZE_OPTION = 's'
@@ -48,7 +49,7 @@ def audio_filelist(root_dir):
     fullfilelist = []
     for dir_name, _, file_list in os.walk(root_dir):
         for file in file_list:
-            if (is_audio_file(file)):
+            if is_audio_file(file):
                 fullfilelist.append(dir_name + r'\\' + file)
     return fullfilelist
 
@@ -58,7 +59,7 @@ def video_filelist(root_dir):
     fullfilelist = []
     for dir_name, _, file_list in os.walk(root_dir):
         for file in file_list:
-            if (is_video_file(file)):
+            if is_video_file(file):
                 fullfilelist.append(dir_name + r'\\' + file)
     return fullfilelist
 
@@ -68,7 +69,7 @@ def image_filelist(root_dir):
     fullfilelist = []
     for dir_name, _, file_list in os.walk(root_dir):
         for file in file_list:
-            if (is_image_file(file)):
+            if is_image_file(file):
                 fullfilelist.append(dir_name + r'\\' + file)
     return fullfilelist
 
@@ -78,14 +79,14 @@ def subdir_list(root_dir):
     fullfilelist = []
     for _, _, file_list in os.walk(root_dir):
         for file in file_list:
-            if (os.path.isdir(file)):
+            if os.path.isdir(file):
                 fullfilelist.append(file)
     return fullfilelist
 
 def get_file_extension(filename):
     """Returns a file extension"""
     return re.sub(r'^.*\.', '', filename)
-  
+
 def strip_file_extension(filename):
     """Removes the file extension and returns the string"""
     return re.sub(r'\.[^.]+$', '', filename)
@@ -103,15 +104,15 @@ def add_postfix(file, postfix, extension = None):
 
 def is_audio_file(file):
     """Returns whether the file has an extension corresponding to audio files"""
-    return match_extension(file,  r'\.(mp3|ogg|aac|ac3|m4a|ape)$')
+    return match_extension(file, r'\.(mp3|ogg|aac|ac3|m4a|ape)$')
 
 def is_video_file(file):
     """Returns whether the file has an extension corresponding to video files"""
-    return match_extension(file,  r'\.(avi|wmv|mp4|3gp|mpg|mpeg|mkv|ts|mts|m2ts)$')
+    return match_extension(file, r'\.(avi|wmv|mp4|3gp|mpg|mpeg|mkv|ts|mts|m2ts)$')
 
 def is_image_file(file):
     """Returns whether the file has an extension corresponding to images files"""
-    return match_extension(file,  r'\.(jpg|jpeg|png|gif|svg|raw)$')
+    return match_extension(file, r'\.(jpg|jpeg|png|gif|svg|raw)$')
 
 def is_media_file(file):
     """Returns whether the file has an extension corresponding to media (audio/video/image) files"""
@@ -134,7 +135,7 @@ def get_properties(props_file = None):
     global PROPERTIES_FILE
     if props_file is None:
         props_file = PROPERTIES_FILE
-    
+
     with open(props_file) as fp:
         properties = jprops.load_properties(fp)
     return properties
@@ -145,18 +146,73 @@ def to_hms(seconds):
     minutes = int(s)//60 - hours*60
     secs = s - hours*3600 - minutes*60
     return (hours, minutes, secs)
-    
+
 def to_hms_str(seconds):
     hours, minutes, secs = to_hms(seconds)
     return "%d:%02d:%06.3f" % (hours, minutes, secs)
 
 def set_debug_level(level):
-    global debug_level
+    global DEBUG_LEVEL
     if level is None:
         level = 0
-    debug_level = int(level)
+    DEBUG_LEVEL = int(level)
 
 def debug(level, string):
-    global debug_level
-    if level <= debug_level:
+    global DEBUG_LEVEL
+    if level <= DEBUG_LEVEL:
         print(string)
+
+def parse_common_args(desc):
+    """Parses options common to all media encoding scripts"""
+    try:
+        import argparse
+    except ImportError:
+        if sys.version_info < (2, 7, 0):
+            print("""Error: You are running an old version of python. Two options to fix the problem
+                   Option 1: Upgrade to python version >= 2.7
+                   Option 2: Install argparse library for the current python version
+                             See: https://pypi.python.org/pypi/argparse""")
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('-i', '--inputfile', required=True, help='Input File or Directory to encode')
+    parser.add_argument('-o', '--outputfile', required=False, help='Output file or directory')
+    parser.add_argument('-p', '--profile', required=False, help='Profile to use for encoding')
+    parser.add_argument('-t', '--ranges', required=False, help='Time ranges to encode')
+    parser.add_argument('-f', '--format', required=False, help='Output file format')
+    parser.add_argument('-r', '--fps', required=False, help='Video framerate of the output')
+    parser.add_argument('--acodec', required=False, help='Audio codec (mp3, aac, ac3...)')
+    parser.add_argument('--abitrate', required=False, help='Audio bitrate')
+    parser.add_argument('--asampling', required=False, help='Audio sampling')
+    parser.add_argument('--vcodec', required=False, help='Video codec (h264, h265, mp4, mpeg2, xvid...)')
+    parser.add_argument('--vsize', required=False, help='Video size HxW')
+    parser.add_argument('--vbitrate', required=False, help='Video bitrate')
+    parser.add_argument('--aspect', required=False, help='Aspect Ratio 16:9, 4:3, 1.5 ...')
+    parser.add_argument('--ranges', required=False, help='Ranges of encoding <start>:<end>,<start>:<end>')
+    parser.add_argument('-g', '--util.debug', required=False, help='util.debug level')
+    return parser
+
+def cleanup_options(options):
+    new_options = options.copy()
+    for key in ['inputfile', 'outputfile', 'profile', 'debug']:
+        del new_options[key]
+    return new_options
+
+def get_media_properties(props_file = None):
+    """Returns all properties found in the properties file as dictionary"""
+    try:
+        properties = get_properties(props_file)
+    except FileNotFoundError:
+        properties['binaries.ffmpeg'] = 'ffmpeg'
+        properties['binaries.ffprobe'] = 'ffprobe'
+    return properties
+
+def get_profile_extension(profile, properties = None):
+    if properties is None:
+        properties = get_media_properties()
+    try:
+        extension = properties[profile + '.extension']
+    except KeyError:
+        try:
+            extension = properties['default.extension']
+        except KeyError:
+            extension = None
+    return extension
