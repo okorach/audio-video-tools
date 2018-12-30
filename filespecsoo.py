04e7bf6c0148f312ef0a35a297620cadfb11564a
Olivier Korach <olivier.korach@gmail.com>
2018-12-30 11:21:44 +0100

    Manage units

--- filespecsoo.py
+++ filespecsoo.py
@@ -19,14 +19,17 @@ else:
 
 is_first = True
 
-props = ['filename', 'filesize', 'type', 'format', 'width', 'height', 'duration', \
+PROPS = ['filename', 'filesize', 'type', 'format', 'width', 'height', 'duration', \
     'video_codec', 'video_bitrate', 'aspect_ratio', 'pixel_aspect_ratio', 'video_fps', \
     'audio_codec', 'audio_bitrate', 'audio_sample_rate',  \
     'author', 'title', 'album', 'year', 'track', 'genre']
 
+UNITS = { 'filesize' : [1048576, 'MB'], 'duration':[1,'hms'], 'video_bitrate':[1024, 'kbits/s'], \
+          'audio_bitrate':[1024, 'kbits/s'], 'audio_sample_rate':[1000, 'k'], }
+
 if args.format != 'txt':
     print("# ")
-    for prop in props:
+    for prop in PROPS:
         print("%s;" % prop, end='')
     print('')
 
@@ -43,13 +46,16 @@ for file in filelist:
         else:
             file_object = videotools.videofile.MediaFile(file)
         specs = file_object.get_properties()
-        for prop in props:
+        for prop in PROPS:
             if args.format == "txt":
                 try:
-                    if prop is 'duration':
-                        print("%-20s : %s" % (prop, videotools.videofile.to_hms_str(specs[prop])))
-                    elif prop is 'filesize':
-                        print("%-20s : %.1f MB" % (prop, videotools.filetools.to_m(specs[prop])))
+                    if prop in UNITS.keys():
+                        divider = UNITS[prop][0]
+                        unit = UNITS[prop][1]
+                        if unit is 'hms':
+                            print("%-20s : %s" % (prop, videotools.videofile.to_hms_str(specs[prop])))
+                        else:
+                            print("%-20s : %.1f %s" % (prop, (int(specs[prop])/divider), unit))
                     else:
                         print("%-20s : %s" % (prop, str(specs[prop]) if specs[prop] is not None else ''))
                 except KeyError:
