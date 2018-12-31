87e4eb775b6a88a36786f4840d2cd528870b092b
Olivier Korach <olivier.korach@gmail.com>
2018-12-31 11:47:37 +0100

    cleanup

--- encode.py
+++ encode.py
@@ -3,9 +3,52 @@
 import sys
 import os
 import re
+from shutil import copyfile
 import mediatools.videofile as video
 import mediatools.utilities as util
 import mediatools.mediafile as media
+
+def encode_file(args, options):
+    if args.timeranges is None:
+        video.encodeoo(args.inputfile, args.outputfile, args.profile, **options)
+        return
+    if args.outputfile is None:
+        ext = util.get_profile_extension(args.profile)
+    count = 0
+    for video_range in re.split(',', args.timeranges):
+        options['ss'], options['to'] = re.split('-', video_range)
+        count += 1
+        if args.outputfile is None:
+            target_file = util.add_postfix(args.inputfile, str(count), ext)
+        else:
+            target_file = args.outputfile
+        video.encodeoo(args.inputfile, target_file, args.profile, **options)
+
+def encode_dir(args, options):
+    targetdir = args.inputfile + '.' + args.profile
+    util.debug(1, "%s ==> %s" % (args.inputfile, targetdir))
+    try:
+        os.mkdir(targetdir)
+    except FileExistsError:
+        pass
+    ext = util.get_profile_extension(args.profile)
+    filelist = util.filelist(args.inputfile)
+    nbfiles = len(filelist)
+    i = 0
+    for fname in filelist:
+        util.debug(0, "%5d/%5d : %3d%% : %s" % (i, nbfiles, round(i * 100 / nbfiles), fname))
+        targetfname = fname.replace(args.inputfile, targetdir, 1)
+        if util.is_audio_file(fname) or util.is_video_file(fname):
+            targetfname = util.strip_file_extension(targetfname) + r'.' + ext
+            directory = os.path.dirname(targetfname)
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            video.encode(fname, targetfname, args.profile, **options)
+        else:
+            copyfile(fname, targetfname)
+            util.debug(2, "Skipping/Plain Copy %s" % (fname))
+        i = i + 1
+    util.debug(0, '%05d/%05d : 100%% : Job finished' % (nbfiles, nbfiles))
 
 parser = util.parse_common_args('Audio and Video file (re)encoder')
 args = parser.parse_args()
@@ -13,47 +56,6 @@ util.set_debug_level(int(args.debug))
 options = util.cleanup_options(vars(args))
 
 if os.path.isdir(args.inputfile):
-    targetdir = args.inputfile + '.' + args.profile
-    try:
-        os.mkdir(targetdir)
-    except FileExistsError:
-        pass
-    ext = util.get_profile_extension(args.profile)
-    util.debug(1, "%s ==> %s" % (args.inputfile, targetdir))
-    filelist = util.filelist(args.inputfile)
-    nbfiles = len(filelist)
-    i = 0
-    for fname in filelist:
-        source_extension =  util.get_file_extension(fname)
-        print ("%5d/%5d : %3d%% : " % (i, nbfiles, round(i * 100 / nbfiles)))
-        if util.is_audio_file(fname) or util.is_video_file(fname):
-            targetfname = fname.replace(args.inputfile, targetdir, 1)
-            targetfname = util.strip_file_extension(targetfname) + r'.' + ext
-            directory = os.path.dirname(targetfname)
-            if not os.path.exists(directory):
-                os.makedirs(directory)
-            video.encode(fname, targetfname, args.profile, **options)
-        else:
-            from shutil import copyfile
-            targetfname = fname.replace(args.inputfile, targetdir, 1)
-            copyfile(fname, targetfname)
-            print("Skipping/Plain Copy %s" % (fname))
-        i = i + 1
-    print ('100%: Job finished')
+    encode_dir(args, options)
 else:
-    if args.timeranges is None:
-        video.encodeoo(args.inputfile, args.outputfile, args.profile, **options)
-    else:
-        if args.outputfile is None:
-            ext = util.get_profile_extension(args.profile)
-        count = 0
-        for video_range in re.split(',', args.timeranges):
-            start, stop = re.split('-', video_range)
-            options['ss'] = start
-            options['to'] = stop
-            count = count + 1
-            if args.outputfile is None:
-                target_file = util.add_postfix(args.inputfile, str(count), ext)
-            else:
-                target_file = args.outputfile
-            video.encodeoo(args.inputfile, target_file, args.profile, **options)
+    encode_file(args, options)
