1400c36b8a6faf4c58f5e48a2f43ba829f2d9f6f
Olivier Korach <olivier.korach@gmail.com>
2018-12-31 10:17:56 +0100

    OO +  cleanup

--- crop.py
+++ crop.py
@@ -1,12 +1,12 @@
 #!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3
 
 import argparse
-from mediatools.videofile import crop
+from mediatools.videofile import cropoo
 from mediatools.utilities import debug, set_debug_level
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Apply deshake filter')
-    parser.add_argument('-i', '--inputfile', required=True, help='Input File or Directory to encode')
+    parser = argparse.ArgumentParser(description='Crops a region of the input video file')
+    parser.add_argument('-i', '--inputfile', required=True, help='Input file or directory to crop')
     parser.add_argument('-o', '--outputfile', required=False, help='Output file to generate')
     parser.add_argument('-g', '--debug', required=False, help='util.debug level')
     parser.add_argument('-b', '--box', required=True, help='Video box')
@@ -18,5 +18,10 @@ args = parse_args()
 set_debug_level(args.debug)
 
 width, height = args.box.split("x")
-outputfile = crop(args.inputfile, int(width), int(height), int(args.top), int(args.left), args.outputfile)
+kwargs = vars(args).copy()
+for key in ['inputfile', 'outputfile', 'box', 'debug', 'left', 'top']:
+    del kwargs[key]
+
+outputfile = cropoo(args.inputfile, int(width), int(height), int(args.top), int(args.left), \
+    args.outputfile, **kwargs)
 debug(1, 'Generated %s' % outputfile)
