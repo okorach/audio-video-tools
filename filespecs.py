9bb89ea703d92d4349b4a0f3f89cd04a1f8acb3d
Olivier Korach <olivier.korach@gmail.com>
2018-12-31 10:36:53 +0100

    Rename non OO by OO

--- filespecs.py
+++ filespecs.py
@@ -1,12 +1,13 @@
-#!python3
+#!/usr/local/bin/python3
 
 import sys
 import os
 import re
 import argparse
-import mediatools.videofile as video
-import mediatools.mediafile as media
 import mediatools.utilities as util
+import mediatools.videofile as video
+import mediatools.audiofile as audio
+import mediatools.mediafile as media
 
 parser = util.parse_common_args('Audio/Video/Image file specs extractor')
 args = parser.parse_args()
@@ -21,14 +22,17 @@ else:
 
 is_first = True
 
-props = ['filename', 'filesize', 'type', 'format', 'width', 'height', 'duration', 'duration_hms', \
-    'video_codec', 'video_bitrate', 'video_aspect_ratio', 'video_fps', \
+PROPS = ['filename', 'filesize', 'type', 'format', 'width', 'height', 'duration', \
+    'video_codec', 'video_bitrate', 'aspect_ratio', 'pixel_aspect_ratio', 'video_fps', \
     'audio_codec', 'audio_bitrate', 'audio_sample_rate',  \
     'author', 'title', 'album', 'year', 'track', 'genre']
 
-if args.format != 'txt':
+UNITS = { 'filesize' : [1048576, 'MB'], 'duration':[1,'hms'], 'video_bitrate':[1024, 'kbits/s'], \
+          'audio_bitrate':[1024, 'kbits/s'], 'audio_sample_rate':[1000, 'k'], }
+
+if args.format == 'csv':
     print("# ")
-    for prop in props:
+    for prop in PROPS:
         print("%s;" % prop, end='')
     print('')
 
@@ -36,17 +40,35 @@ for file in filelist:
     if not util.is_media_file(file):
         continue
     try:
-        myspecs = video.get_file_specs(file)
-        for prop in props:
-            if args.format == "txt":
+        if util.is_video_file(file):
+            file_object = video.VideoFile(file)
+        elif util.is_audio_file(file):
+            file_object = audio.AudioFile(file)
+        elif util.is_image_file(file):
+            file_object = media.MediaFile(file)
+        else:
+            file_object = media.MediaFile(file)
+        specs = file_object.get_properties()
+        for prop in PROPS:
+            if args.format != "csv":
                 try:
-                    print("%-20s : %s" % (prop, str(myspecs[prop])))
+                    if prop in UNITS.keys():
+                        divider = UNITS[prop][0]
+                        unit = UNITS[prop][1]
+                        if unit is 'hms':
+                            print("%-20s : %s" % (prop, util.to_hms_str(specs[prop])))
+                        else:
+                            print("%-20s : %.1f %s" % (prop, (int(specs[prop])/divider), unit))
+                    else:
+                        print("%-20s : %s" % (prop, str(specs[prop]) if specs[prop] is not None else ''))
                 except KeyError:
                     print("%-20s : %s" % (prop, ""))
             else:
                 # CSV format
                 try:
-                    print("%s;" % str(myspecs[prop]), end='')
+                    print("%s;" % (str(specs[prop]) if specs[prop] is not None else ''), end='')
+                    if prop is 'duration':
+                        print("%s;" % util.to_hms_str(specs[prop]))
                 except KeyError:
                     print("%s;" % '', end='')
         print('')
