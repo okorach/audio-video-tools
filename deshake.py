6150bc0cf5228027a5dd8b8486051f4027db8384
Olivier Korach <olivier.korach@gmail.com>
2019-01-05 17:44:59 +0100

    Implement cropping after deshake

--- deshake.py
+++ deshake.py
@@ -1,17 +1,24 @@
 #!/usr/local/bin/python3
 
+import re
 import argparse
 import mediatools.utilities as util
 from mediatools.videofile import deshake
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Apply deshake filter')
-    parser.add_argument('-i', '--inputfile', required=True, help='Input File or Directory to encode')
-    parser.add_argument('-o', '--outputfile', required=False, help='Output file to generate')
-    parser.add_argument('-w', '--width', required=True, help='Deshake width')
-    parser.add_argument('-x', '--height', required=True, help='Deshake height')
+    parser = util.parse_common_args(desc='Apply deshake filter')
+    parser.add_argument('--width', required=True, help='Deshake width')
+    parser.add_argument('--height', required=True, help='Deshake height')
+    parser.add_argument('--nocrop', required=False, help='Do not crop video after deshaking')
     return parser.parse_args()
 
 args = parse_args()
-outputfile = deshake(args.inputfile, int(args.width), int(args.height), args.outputfile)
+util.set_debug_level(args.debug)
+kwargs = util.cleanup_options(vars(args))
+del kwargs['width']
+del kwargs['height']
+if args.timeranges is not None:
+    for video_range in re.split(',', args.timeranges):
+        kwargs['ss'], kwargs['to'] = re.split('-', video_range)
+outputfile = deshake(args.inputfile, int(args.width), int(args.height), args.outputfile, **kwargs)
 util.debug(1, 'Generated %s' % outputfile)
