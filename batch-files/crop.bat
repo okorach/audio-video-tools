a7ca6fd264ffee348251b0302800e44bd4b48ecf
Olivier Korach <olivier.korach@gmail.com>
2019-06-09 09:29:40 +0200

    Remove command path

--- batch-files/crop.bat
+++ batch-files/crop.bat
@@ -1,3 +1,3 @@
 
-E:\Backup\Dev\audio-video-tools\crop.py -i %1 --box 1100x720 --top 0 --left 0 -g 2
+crop.py -i %1 --box 1100x720 --top 0 --left 0 -g 2
 set /p debug=Enter to quit:
--- batch-files/thumbnails.bat
+++ batch-files/thumbnails.bat
@@ -1,4 +1,3 @@
 
 
-E:\Backup\Dev\audio-video-tools\encode.py -i %1 -p thumbnails
-set /p debug=Enter to quit ?:
+encode.py -i %1 -p thumbnails
