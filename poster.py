e4a13852b695fb70ed8e6aec79f1a7e188fc6fd4
Olivier Korach <olivier.korach@gmail.com>
2019-06-09 18:21:16 +0200

    add margin

--- poster.py
+++ poster.py
@@ -8,23 +8,26 @@ import mediatools.imagefile as image
 
 DEFAULT_RESCALING = '512x512'
 
-def filelist_poster(filelist, bg):
-    image.posterize(filelist, None, bg)
+def filelist_poster(filelist, bg, margin):
+    image.posterize(filelist, None, bg, margin)
 
-def dir_poster(directory, bg):
+def dir_poster(directory, bg, margin):
     filelist = util.filelist(directory)
-    filelist_poster(filelist, bg)
+    filelist_poster(filelist, bg, margin)
 
 file_list = []
 dir_list = []
 bg = "black"
+margin=5
 sys.argv.pop(0) # Remove script name
 while len(sys.argv) > 0:
     arg = sys.argv.pop(0)
     if arg == "-g":
         util.set_debug_level(sys.argv.pop(0))
-    elif arg is "-background":
+    elif arg == "-background":
         bg = sys.argv.pop(0)
+    elif arg == "-margin":
+        margin = int(sys.argv.pop(0))
     elif os.path.isdir(arg):
         dir_list.append(arg)
     else:
@@ -35,4 +38,4 @@ for directory in dir_list:
     dir_poster(directory)
 
 if len(file_list) > 0:
-    filelist_poster(file_list, bg)
+    filelist_poster(file_list, bg, margin)
