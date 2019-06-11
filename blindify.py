a14ecd4d55870a81aa916eaab665344e8b48edf8
Olivier Korach <olivier.korach@gmail.com>
2019-06-12 01:48:59 +0400

    New scripts

--- blindify.py
+++ blindify.py
@@ -7,10 +7,10 @@ import mediatools.imagefile as image
 parser = argparse.ArgumentParser(description='Image cropper')
 parser.add_argument('-i', '--inputfile', required=True, help='Input file to crop')
 parser.add_argument('-o', '--outputfile', required=False, help='Output file to create')
-parser.add_argument('--align', required=False, default='center', help='How to crop')
-parser.add_argument('--ratio', required=True, help='W/H ratio of picture like 2, 1.5')
+parser.add_argument('--blinds', required=False, default=10, help='Number of blinds')
+parser.add_argument('--blinds_ratio', required=False, default=5, help='Size of the blind')
 parser.add_argument('-g', '--debug', required=False, default=0, help='Debug level')
 args = parser.parse_args()
 
 util.set_debug_level(args.debug)
-image.ImageFile(args.inputfile).crop_any(args.ratio, args.align)
+image.ImageFile(args.inputfile).blindify(int(args.blinds), float(args.blinds_ratio))
