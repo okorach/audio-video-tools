32006dbcbfc7837a460e53e9a428798f03a779b8
Olivier Korach <olivier.korach@gmail.com>
2019-07-09 08:24:49 +0200

    First commit

--- video_cut.py
+++ video_cut.py
@@ -5,8 +5,10 @@ import mediatools.videofile as video
 import mediatools.utilities as util
 
 parser = util.parse_common_args('Cuts a time window of the input video file')
-args = parser.parse_args()
-util.set_debug_level(args.debug)
-
-outputfile = video.VideoFile(args.inputfile).cut(args.inputfile, args.start, args.stop, **kwargs)
+kwargs = vars(parser.parse_args())
+util.set_debug_level(dict.pop(kwargs, 'debug'))
+start = dict.pop(kwargs, 'start')
+stop = dict.pop(kwargs, 'stop')
+ifile = dict.pop(kwargs, 'inputfile')
+outputfile = video.VideoFile(ifile).cut(start, stop, **kwargs)
 util.debug(1, 'Generated %s' % outputfile)
