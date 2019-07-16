c9c1777bec2aadfb776beb8f58a48ded0c47fa5f
Olivier Korach <olivier.korach@gmail.com>
2019-07-16 16:06:38 +0200

    Common args

--- crop.py
+++ crop.py
@@ -5,6 +5,7 @@ import mediatools.videofile as video
 import mediatools.utilities as util
 
 parser = util.parse_common_args('Crops a region of the input video file')
+parser = video.add_video_args(parser)
 parser.add_argument('--box', required=True, help='Video box eg 320x200')
 parser.add_argument('--top', required=True, help='Video top origin')
 parser.add_argument('--left', required=True, help='Video left origin')
@@ -14,7 +15,7 @@ util.set_debug_level(args.debug)
 width, height = args.box.split("x")
 kwargs = vars(args).copy()
 for key in ['inputfile', 'outputfile', 'box', 'debug', 'left', 'top']:
-    del kwargs[key]
+    kwargs.pop(key, 0)
 
 outputfile = video.crop(args.inputfile, int(width), int(height), int(args.top), int(args.left), \
     args.outputfile, **kwargs)
