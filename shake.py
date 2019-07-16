a2d6a34cf9eda4c16f1a5bd03ac13f43fb620cf1
Olivier Korach <olivier.korach@gmail.com>
2019-07-16 16:08:39 +0200

    common args

--- shake.py
+++ shake.py
@@ -4,15 +4,11 @@ import argparse
 import mediatools.utilities as util
 import mediatools.imagefile as image
 
-parser = argparse.ArgumentParser(description='Image cropper')
-parser.add_argument('-i', '--inputfile', required=True, help='Input file to crop')
-parser.add_argument('-o', '--outputfile', required=False, help='Output file to create')
+parser = util.parse_common_args('Image shake effect')
 parser.add_argument('-n', '--slices', required=False, default=10, help='Number of slices')
 parser.add_argument('-d', '--direction', required=False, default='vertical', help='Direction to slice')
 parser.add_argument('-c', '--color', required=False, default='black', help='Blinds color')
 parser.add_argument('-r', '--shake_ratio', required=False, default=3, help='Size of the shake')
-parser.add_argument('-g', '--debug', required=False, default=0, help='Debug level')
-parser.add_argument('--dry_run', required=False, default='no', help='Dry run')
 args = parser.parse_args()
 
 util.set_debug_level(args.debug)
