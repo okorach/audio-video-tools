fbd96d85365c34ce5f157f87cf0f423542a8cb01
Olivier Korach <olivier.korach@gmail.com>
2019-07-16 16:08:25 +0200

    compon args

--- rescale.py
+++ rescale.py
@@ -7,11 +7,8 @@ import argparse
 import mediatools.imagefile as image
 import mediatools.utilities as util
 
-parser = argparse.ArgumentParser(description='Rescale image dimensions of a file or directory')
-parser.add_argument('-i', '--inputfile', required=True, help='Input File or Directory to encode')
-parser.add_argument('-o', '--outputfile', required=False, help='Output file to generate')
+parser = util.parse_common_args('Rescale image dimensions of a file or directory')
 parser.add_argument('-s', '--scale', required=True, help='Dimensions to rescale widthxheight')
-parser.add_argument('-g', '--debug', required=False, default=0, help='Debug level')
 
 args = parser.parse_args()
 util.set_debug_level(args.debug)
