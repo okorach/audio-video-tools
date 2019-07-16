51b6d3a1c7c0c5c9411bc0f93245c0541630453c
Olivier Korach <olivier.korach@gmail.com>
2019-07-16 16:18:01 +0200

    bug fix

---
+++ video_cut.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python3
+
+import argparse
+import mediatools.videofile as video
+import mediatools.utilities as util
+
+parser = util.parse_common_args('Cuts a time window of the input video file')
+kwargs = vars(parser.parse_args())
+util.set_debug_level(kwargs.pop('debug', 0))
+start = kwargs.pop('start', None)
+stop = kwargs.pop('stop', None)
+ifile = kwargs.pop('inputfile')
+outputfile = video.VideoFile(ifile).cut(start, stop, **kwargs)
+util.debug(1, 'Generated %s' % outputfile)
