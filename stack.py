7dd84b5dbcb94ba07054a5c1b561fc63638d47d2
Olivier Korach <olivier.korach@gmail.com>
2019-07-16 16:08:56 +0200

    common args

--- stack.py
+++ stack.py
@@ -7,13 +7,11 @@ import argparse
 import mediatools.imagefile as image
 import mediatools.utilities as util
 
-parser = argparse.ArgumentParser(description='Sticks 2 image together')
+parser = util.parse_common_args('Sticks 2 image together')
 parser.add_argument('-i1', '--inputfile1', required=True, help='Input File 1')
 parser.add_argument('-i2', '--inputfile2', required=True, help='Input File 2')
-parser.add_argument('-o', '--outputfile', required=False, help='Output file to generate')
 parser.add_argument('-d', '--direction', required=False, default='vertical', \
     help='Stacking direction (horizontal or vertical)')
-parser.add_argument('-g', '--debug', required=False, default=0, help='Debug level')
 
 args = parser.parse_args()
 util.set_debug_level(args.debug)
