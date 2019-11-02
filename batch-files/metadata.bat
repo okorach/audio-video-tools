45bee357463a8c1dfbffe917df8ac1da162c3a85
Olivier Korach <olivier.korach@gmail.com>
2019-11-02 08:44:56 +0100

    take into account several audio tracks

--- batch-files/metadata.bat
+++ batch-files/metadata.bat
@@ -1,5 +1,5 @@
 set /p year=Year of video?:
 
-E:\Tools\ffmpeg\bin\ffmpeg -i "%~1" -metadata year=%year% -metadata copyright="(c) O. Korach %year%" -c copy  -metadata author="Olivier Korach" -metadata:s:a:0 language=fre -metadata:s:v:0 language=fre "%~1.meta.mp4"
+E:\Tools\ffmpeg\bin\ffmpeg -i "%~1" -vcodec copy -c:a copy -map 0 -metadata year=%year% -metadata copyright="(c) O. Korach %year%"  -metadata author="Olivier Korach" -metadata:s:a:0 language=fre -metadata:s:a:0 title="Avec musique" -metadata:s:a:1 language=fre -metadata:s:a:1 title="Sans musique" -metadata:s:v:0 language=fre -disposition:a:0 default -disposition:a:1 none "%~1.meta.mp4"
 
 pause
