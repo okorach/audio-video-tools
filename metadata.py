bfff115bfc1258223958519fe3e2401c7184814e
Olivier Korach <olivier.korach@gmail.com>
2019-12-24 19:03:45 +0100

    default track handling

--- metadata.py
+++ metadata.py
@@ -8,7 +8,7 @@ parser = util.parse_common_args('Tool to add metadata to media files')
 parser.add_argument('--copyright', required=False, help='Copyright string without year')
 parser.add_argument('--author', required=False, help='Author of the media file')
 parser.add_argument('--year', required=False, help='Year the media file was produced')
-parser.add_argument('--default-track', required=False, help='Default track')
+parser.add_argument('--default_track', required=False, type=int, help='Default track')
 parser.add_argument('--languages', required=False, nargs='+', help='Languages of tracks, eg 0:fre 1:eng')
 parser.add_argument('--titles', required=False, nargs='+', help='Titles of tracks, eg "0:French canadian" "1:English with music"')
 kwargs = vars(parser.parse_args())
@@ -21,6 +21,8 @@ if util.is_video_file(inputfile):
              'year': kwargs.pop('year', None)
             }
     inputfile = video.VideoFile(inputfile).add_metadata(**metas)
+    if 'default_track' in kwargs:
+        inputfile = video.VideoFile(inputfile).set_default_track(kwargs['default_track'])
     for opt in ['languages', 'titles']:
         if opt not in kwargs:
             continue
@@ -30,7 +32,5 @@ if util.is_video_file(inputfile):
             vals[idx] = val
         if opt == 'languages':
             inputfile = video.VideoFile(inputfile).set_tracks_language(**vals)
-        elif opt == 'title':
-            inputfile = video.VideoFile(inputfile).set_tracks_language(**vals)
-    if 'default-track' in kwargs:
-        inputfile = video.VideoFile(inputfile).set_default_track(kwargs['default-track'])
+        elif opt == 'titles':
+            inputfile = video.VideoFile(inputfile).set_tracks_title(**vals)
