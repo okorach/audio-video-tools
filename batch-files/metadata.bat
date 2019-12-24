363db2e85816f92cddf4a639dd2b3c67cb8c1ff9
Olivier Korach <olivier.korach@gmail.com>
2019-12-24 11:59:22 +0100

    fix metadata

--- batch-files/metadata.bat
+++ batch-files/metadata.bat
@@ -1,5 +1,5 @@
 set /p year=Year of video?:
 
-%~dp0\..\mux.py -i "%~1" --year %year% --copyright "(c) O. Korach %year%"  --author "Olivier Korach" "%~1.meta.mp4"
+%~dp0\..\metadata.py -i "%~1" --year %year% --copyright "(c) O. Korach %year%"  --author "Olivier Korach" "%~1.meta.mp4"
 
 pause
