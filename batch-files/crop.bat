be7666b14af5bc654a579331d6398edda20dc2b4
Olivier Korach <olivier.korach@gmail.com>
2020-05-10 18:12:00 +0200

    added time range adn 720p, 540p boxing

--- batch-files/crop.bat
+++ batch-files/crop.bat
@@ -1,6 +1,10 @@
 set /p box=Box ?:
 set /p top=Top ?:
 set /p left=Left ?:
-%~dp0\..\crop.py -i "%~1" --box %box% --top %left% --left %top% -g 2
-
-rem pause
+set /p range=Range ?:
+if %range% == "" (
+    %~dp0\..\crop.py -i "%~1" --box %box% --top %left% --left %top% -g 2
+) else (
+    %~dp0\..\crop.py -i "%~1" --timerange %range% --box %box% --top %left% --left %top% -g 2
+)
+pause
