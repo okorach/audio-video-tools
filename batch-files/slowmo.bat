0811856eea2157c555a28831babfcc6351190add
Olivier Korach <olivier.korach@gmail.com>
2020-05-10 18:12:14 +0200

    fix ffmpeg path

--- batch-files/slowmo.bat
+++ batch-files/slowmo.bat
@@ -2,5 +2,6 @@ setlocal enabledelayedexpansion
 set /p speed=Speed factor ?:
 Rem -an removes audio
 for %%F in (%*) do (
-    E:\Tools\ffmpeg-20180928-win64\bin\ffmpeg.exe -i "%%~F" -filter:v "setpts=%speed%*PTS" -an -vcodec libx264  "%%~F.slowmo.%speed%.mp4"
+    E:\Tools\ffmpeg\bin\ffmpeg.exe -i "%%~F" -filter:v "setpts=%speed%*PTS" -an -vcodec libx264 -b:v 6000k "%%~F.slowmo.%speed%.mp4"
 )
+pause
