2e35a924c195123875e1a7a7406841cf45cae92f
Olivier Korach <olivier.korach@gmail.com>
2020-05-10 18:11:16 +0200

    fix vwidth

--- batch-files/1080p.bat
+++ batch-files/1080p.bat
@@ -1,4 +1,6 @@
 setlocal enabledelayedexpansion
+
 for %%F in (%*) do (
-    %~dp0\..\encode.py -i "%%~F" -p 1080p --vwidth 1920 --vbitrate 16000k -g 5
+    %~dp0\..\encode.py --hw_accel -i "%%~F" -p 1080p --width 1920 --vbitrate 8192k -g 5
 )
+pause
