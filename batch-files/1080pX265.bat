32cffd038f0154b264c2b158f1dfa581bcd583b6
Olivier Korach <olivier.korach@gmail.com>
2020-05-24 21:02:23 +0200

    Add X265

--- batch-files/1080pX265.bat
+++ batch-files/1080pX265.bat
@@ -1,6 +1,6 @@
 setlocal enabledelayedexpansion
 
 for %%F in (%*) do (
-    %~dp0\..\encode.py --hw_accel -i "%%~F" -p 1080p6mX265 --width 1920 --vbitrate 6144k -g 5
+    %~dp0\..\encode.py --hw_accel -i "%%~F" -p 1080pX265 --width 1920 --vbitrate 6144k -g 5
 )
 pause
