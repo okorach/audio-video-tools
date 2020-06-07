472faf8550e5d4ef5c1e9c6975c86874e1e18300
Olivier Korach <olivier.korach@gmail.com>
2020-06-07 15:57:38 +0200

    fix typo

--- batch-files/1080p.bat
+++ batch-files/1080p.bat
@@ -1,5 +1,5 @@
 setlocal enabledelayedexpansion
 
 for %%F in (%*) do (
-    %~dp0\..\encode.py --hw_accel -i "%%~F" -p 1080p --width 1920 --vbitrate 8192k "%%~F.1080p.mp4"
+    %~dp0\..\encode.py --hw_accel -i "%%~F" -p 1080p --width 1920 --vbitrate 8192k -o "%%~F.1080p.mp4"
 )
