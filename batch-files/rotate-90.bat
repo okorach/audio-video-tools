211e84bc4f297b4ee110d15b76d7306d101f2abc
Olivier Korach <olivier.korach@gmail.com>
2020-06-07 15:57:56 +0200

    Support more than 1 file

--- batch-files/rotate-90.bat
+++ batch-files/rotate-90.bat
@@ -1,4 +1,5 @@
-E:\Tools\ffmpeg\bin\ffmpeg -i "%~1" -f mp4 -acodec aac -b:a 128k -vcodec libx264 -b:v 10000k -vf "transpose=1" "%~1.rotate90.mp4"
-
-pause
+setlocal enabledelayedexpansion
+for %%F in (%*) do (
+    E:\Tools\ffmpeg\bin\ffmpeg -i "%%~F" -f mp4 -acodec aac -b:a 128k -vcodec libx264 -b:v 10000k -vf "transpose=1" "%%~F".rotate90.mp4"
+)
 pause
