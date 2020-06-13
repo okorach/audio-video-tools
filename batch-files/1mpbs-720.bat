833a0f5b5c2dae4518a72be8596a75eeebaf7339
Olivier Korach <olivier.korach@gmail.com>
2020-06-13 09:14:15 +0200

    hw accel

--- batch-files/1mpbs-720.bat
+++ batch-files/1mpbs-720.bat
@@ -1,4 +1,4 @@
 setlocal enabledelayedexpansion
 for %%F in (%*) do (
-    %~dp0\..\encode.py -i "%%~F" -p 2mbps --width 720 --vbitrate 1024k -o "%%~F.1mbps.720x.mp4"
+    %~dp0\..\encode.py -i "%%~F" -p 2mbps --hw_accel --width 720 --vbitrate 1024k -o "%%~F.1mbps.720x.mp4" -g 5
 )
