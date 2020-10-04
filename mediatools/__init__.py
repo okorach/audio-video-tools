1fbfafae924efc022ffe7a1a254064bf184be40a
Olivier Korach <olivier.korach@gmail.com>
2020-10-04 14:47:11 +0200

    Cleanup

--- mediatools/__init__.py
+++ mediatools/__init__.py
@@ -1,6 +1,1 @@
-__all__ = ['utilities', 'mediafile', 'videofile', 'audiofile']
-# deprecated to keep older scripts who import this from breaking
-import mediatools.utilities as util
-import mediatools.videofile as video
-import mediatools.audiofile as audio
-import mediatools.mediafile as media
+
