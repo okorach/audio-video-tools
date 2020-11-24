4ce6437b99961a89be8dcc779db2029dffcf73da
Olivier Korach <olivier.korach@gmail.com>
2020-11-24 13:36:12 +0100

    Added main entry point

--- mediatools/crop_ratio.py
+++ mediatools/crop_ratio.py
@@ -6,11 +6,17 @@ import argparse
 import mediatools.utilities as util
 import mediatools.imagefile as image
 
-parser = argparse.ArgumentParser(description='Image cropper')
-parser = util.parse_common_args('Image cropper')
-parser.add_argument('--align', required=False, default='center', help='How to crop')
-parser.add_argument('--ratio', required=True, help='W/H ratio of picture like 2, 1.5')
-args = parser.parse_args()
 
-util.check_environment(vars(args))
-image.ImageFile(args.inputfile).crop_any(args.ratio, args.align)
+def main():
+    parser = argparse.ArgumentParser(description='Image cropper')
+    parser = util.parse_common_args('Image cropper')
+    parser.add_argument('--align', required=False, default='center', help='How to crop')
+    parser.add_argument('--ratio', required=True, help='W/H ratio of picture like 2, 1.5')
+    args = parser.parse_args()
+
+    util.check_environment(vars(args))
+    image.ImageFile(args.inputfile).crop_any(args.ratio, args.align)
+
+
+if __name__ == "__main__":
+    main()
