5f808ab7b13c550b3ccd2c15262da139f84817ca
Olivier Korach <olivier.korach@gmail.com>
2020-12-26 18:07:52 +0100

    Cleanup

--- mediatools/crop.py
+++ mediatools/crop.py
@@ -33,11 +33,11 @@ def main():
     parser = util.parse_common_args('Crops a region of the input video file')
     parser = video.add_video_args(parser)
     parser.add_argument('--box', required=True, help='Video box eg 320x200')
-    parser.add_argument('--center', required=False, help='Crop video on center of origin video')
+    parser.add_argument('--position', required=False, help='Crop video position (from top-left to bottom-right)')
     parser.add_argument('--top', required=False, help='Video top origin')
     parser.add_argument('--left', required=False, help='Video left origin')
     args = parser.parse_args()
-    kwargs = vars(args).copy()
+    kwargs = util.remove_nones(vars(args).copy())
     util.check_environment(kwargs)
 
     if args.box == '720p':
@@ -50,22 +50,15 @@ def main():
     if args.timeranges is not None:
         (kwargs[opt.media.START], kwargs[opt.media.STOP]) = args.timeranges.split('-')
 
-    width, height = [int(x) for x in args.box.split("x")]
-    file_o = video.VideoFile(args.inputfile)
-    if args.center is not None and args.center == "true":
-        i_w, i_h = file_o.get_dimensions()
-        left = (i_w - width) // 2
-        top = (i_h - height) // 2
-    else:
-        top = int(args.top)
-        left = int(args.left)
+    width, height = args.box.split("x")
+    kwargs.pop('width', None)
+    kwargs.pop('height', None)
 
     # Remove the explicitly passed arguments
     for key in ['inputfile', 'outputfile', 'box', 'left', 'top', 'center']:
         kwargs.pop(key, None)
 
-    outputfile = video.VideoFile(args.inputfile).crop(width, height, top, left,
-        args.outputfile, **kwargs)
+    outputfile = video.VideoFile(args.inputfile).crop(width, height, args.outputfile, **kwargs)
     util.logger.info('Generated %s', outputfile)
 
 
