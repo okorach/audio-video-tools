a99ea22f1242334728e0ba646ce62be8847a6a6e
Olivier Korach <olivier.korach@gmail.com>
2020-12-27 10:55:04 +0100

    Add media-specs IT

---
+++ it/it-media-specs.sh
@@ -0,0 +1,11 @@
+for f in *.mpg *.jpg
+do
+	media-specs -i $f
+	code=$?
+	if [ $code -ne 0 ]; then
+		2>&1 echo "media-specs -i $f FAILED"
+		exit $code
+	fi
+done
+echo "media-specs SUCCESS"
+exit 0
