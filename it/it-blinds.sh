da212366ccb92c0b89802e6a7156c125f047051f
Olivier Korach <olivier.korach@gmail.com>
2020-12-28 22:37:50 +0100

    RUn less test

--- it/it-blinds.sh
+++ it/it-blinds.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
+
+let i=0
 for f in ./*.jpg
 do
-    for n in 2 5 10 17 29
+    for n in 2 7 11 17 29
     do
-        for d in horizontal vertical
+        for r in 1 3 7
         do
-            for c in black white
-            do
-                for r in 1 3 5 9
-                do
-                    cmd="image-blinds -i $f -n $n -d $d -c $c -r $r"
-                    echo "Running: $cmd"
-                    $cmd
-                    code=$?
-                    if [ $code -ne 0 ]; then
-                        1>&2 echo "FAILED: $cmd"
-                        exit $code
-                    fi
-                done
-            done
+            let i=$i+1
+            let k=(3*$i+1)%2
+            if [ "$k" == "0" ]; then
+                d=horizontal
+            else
+                d=vertical
+            fi
+            let k=(7*$i)%2
+            if [ "$k" == "0" ]; then
+                c=black
+            else
+                c=white
+            fi
+            cmd="image-blinds -i $f -n $n -d $d -c $c -r $r"
+            echo "Running: $cmd"
+            $cmd
+            code=$?
+            if [ $code -ne 0 ]; then
+                1>&2 echo "FAILED: $cmd"
+                exit $code
+            fi
         done
     done
 done
