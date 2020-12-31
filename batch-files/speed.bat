9f0f34d35481f00a78f262883342ca5486c1132a
Olivier Korach <olivier.korach@gmail.com>
2020-12-31 12:06:10 +0100

    Change slowmo to speed, base on video-speed

--- batch-files/speed.bat
+++ batch-files/speed.bat
@@ -20,7 +20,6 @@
 
 setlocal enabledelayedexpansion
 set /p speed=Speed factor ?:
-Rem -an removes audio
 for %%F in (%*) do (
-    E:\Tools\ffmpeg\bin\ffmpeg.exe -i "%%~F" -filter:v "setpts=%speed%*PTS" -an -vcodec libx264 -b:v 8000k "%%~F.slowmo.%speed%.mp4"
+    video-speed -i "%%~F" --speed %speed% "%%~F.speed.%speed%.mp4"
 )
