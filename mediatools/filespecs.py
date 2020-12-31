a3ea46dd0bef3ab207844aded103bfe5283a8649
Olivier Korach <olivier.korach@gmail.com>
2020-12-31 23:30:47 +0100

    Quality pass

--- mediatools/filespecs.py
+++ mediatools/filespecs.py
@@ -19,8 +19,6 @@
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
 
-import os
-import re
 import argparse
 import mediatools.exceptions as ex
 import mediatools.utilities as util
