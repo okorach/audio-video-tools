29b48a96b0d86f18d16d979bb82b9b510f85d46b
Olivier Korach <olivier.korach@gmail.com>
2021-01-05 19:34:43 +0100

    Add tests

---
+++ test2/test_resolution.py
@@ -0,0 +1,24 @@
+import unittest
+import mediatools.resolution as res
+
+
+class TestResolution(unittest.TestCase):
+
+    def test_720p(self):
+        self.assertEqual(res.canonical('720p'), '1280x720')
+
+    def test_1080p(self):
+        self.assertEqual(res.canonical('1080p'), '1920x1080')
+
+    def test_4K(self):
+        self.assertEqual(res.canonical('4k'), '1920x1080')
+
+
+if __name__ == '__main__':
+    import xmlrunner
+
+    unittest.main(
+        testRunner=xmlrunner.XMLTestRunner(output='test-reports'),
+        failfast=False,
+        buffer=False,
+        catchbreak=False)
