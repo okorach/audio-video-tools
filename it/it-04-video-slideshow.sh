a5e8a7c95e6745eac040dd197fbec83d8f5adb2c
Olivier Korach <olivier.korach@gmail.com>
2021-01-17 18:31:10 +0100

    Fix bug in IT

--- it/it-04-video-slideshow.sh
+++ it/it-04-video-slideshow.sh
@@ -3,11 +3,11 @@
 . lib-it.sh
 
 res_list="$*"
-if [ "$res_list" == "" ]; then
+if [ "$res_list" == "" ]; then
     res_list="360x200 720x400 1280x720 1920x1080 3840x2160"
 fi
 
-for res in $res_lsit
+for res in $res_list
 do
     run_cmd "video-slideshow *.jpg --resolution $res"
 done
