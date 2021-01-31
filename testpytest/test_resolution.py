e0ced77fe1e4b8a8f6add1984892eb70aad17b79
Olivier Korach <olivier.korach@gmail.com>
2021-01-31 19:19:30 +0100

    Add Resolutions test

--- testpytest/test_resolution.py
+++ testpytest/test_resolution.py
@@ -21,7 +21,7 @@
 #
 
 import mediatools.resolution as res
-
+import mediatools.exceptions as ex
 
 def test_canonical_1():
     assert res.canonical('720p') == '1280x720'
@@ -37,3 +37,25 @@ def test_canonical_3():
 
 def test_canonical_4():
     assert res.canonical('4k') == '3840x2160'
+
+
+def test_multiply():
+    x, y = 1000, 200
+    r = res.Resolution(x, y)
+    r2 = r * 2
+    assert r2.width == x * 2
+    assert r2.height == y * 2
+
+def test_multiply_by_0():
+    r = res.Resolution(300, 17)
+    try:
+        _ = r * 0
+    except ex.NegativeDimensions as e:
+        assert e.message == 'width and height must be strictly positive'
+
+def test_multiply_by_negative():
+    r = res.Resolution(100, 100)
+    try:
+        _ = r * -1
+    except ex.NegativeDimensions as e:
+        assert e.message == 'width and height must be strictly positive'
