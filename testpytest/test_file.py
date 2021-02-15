bdcc0c4cb4000dae9d22b2a800ba6cb023d83af8
Olivier Korach <olivier.korach@gmail.com>
2021-02-15 22:55:44 +0100

    Add test_file.py

---
+++ testpytest/test_file.py
@@ -0,0 +1,58 @@
+#
+# media-tools
+# Copyright (C) 2019-2021 Olivier Korach
+# mailto:olivier.korach AT gmail DOT com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
+import mediatools.file as fil
+import mediatools.videofile as video
+import mediatools.exceptions as ex
+
+FILE = 'it/video-720p.mp4'
+FILE_SIZE = 2001067
+
+def test_file_std():
+    f = fil.File(FILE)
+    assert f.probe() == True
+    assert f.probe() == True
+    assert fil.size == FILE_SIZE
+
+def test_file_video():
+    f = fil.VideoFile(FILE)
+    assert f.probe() == True
+    assert fil.size == FILE_SIZE
+
+def test_file_unexisting():
+    f = fil.File('/tmp/nonexist.txt')
+    assert f.probe() == False
+
+def test_extension():
+    assert fil.extension(FILE) == 'mp4'
+    f = fil.File(FILE)
+    assert f.extension() == 'mp4'
+
+def test_basename():
+    assert fil.basename(FILE) == 'video-720p.mp4'
+    assert fil.basename(FILE, 'mp4') == 'video-720p'
+    f = fil.File(FILE)
+    assert f.basename() == 'video-720p.mp4'
+    assert f.basename('mp4') == 'video-720p'
+
+def test_dirname():
+    assert fil.dirname('/usr/local/bin/python3') == '/usr/local/bin'
+    f = fil.File('/usr/local/bin/python3')
+    assert f.dirname() == '/usr/local/bin'
