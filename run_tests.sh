ae71298e7a817614293aac8fca3e9ce2cb72c595
Olivier Korach <olivier.korach@gmail.com>
2021-09-18 23:33:01 +0200

    Break shells in 3 parts

---
+++ run_tests.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# media-tools
+# Copyright (C) 2021 Olivier Korach
+# mailto:olivier.korach AT gmail DOT com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
+buildDir="build"
+coverageReport="$buildDir/coverage.xml"
+
+[ ! -d $buildDir ] && mkdir $buildDir
+# rm -rf -- ${buildDir:?"."}/* .coverage */__pycache__ */*.pyc # mediatools/__pycache__  testpytest/__pycache__ testunittest/__pycache__
+
+echo "Running tests"
+coverage run -m pytest
+coverage xml -o $coverageReport
