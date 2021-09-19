10fc241455e0a52a112e14e02eefe54365d1d1f0
Olivier Korach <olivier.korach@gmail.com>
2021-09-19 22:54:25 +0200

    version Fix delete old build artefacts

--- deploy.bat
+++ deploy.bat
@@ -17,8 +17,8 @@
 :: along with this program; if not, write to the Free Software Foundation,
 :: Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 ::
-del build/*
-del dist/*
+del /F /Q build\*
+del /F /Q dist\*
 
 python setup.py sdist bdist_wheel
 
