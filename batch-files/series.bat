acb576ce43cbb56707571ab45f8a9a62baf03fee
Olivier Korach <olivier.korach@gmail.com>
2021-11-13 10:57:22 +0100

    New encoding format

---
+++ batch-files/series.bat
@@ -0,0 +1,25 @@
+::
+:: media-tools
+:: Copyright (C) 2019-2021 Olivier Korach
+:: mailto:olivier.korach AT gmail DOT com
+::
+:: This program is free software; you can redistribute it and/or
+:: modify it under the terms of the GNU Lesser General Public
+:: License as published by the Free Software Foundation; either
+:: version 3 of the License, or (at your option) any later version.
+::
+:: This program is distributed in the hope that it will be useful,
+:: but WITHOUT ANY WARRANTY; without even the implied warranty of
+:: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+:: Lesser General Public License for more details.
+::
+:: You should have received a copy of the GNU Lesser General Public License
+:: along with this program; if not, write to the Free Software Foundation,
+:: Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+::
+
+setlocal enabledelayedexpansion
+for %%F in (%*) do (
+    video-encode -i "%%~F" -p 2mbps --width 720 --vcodec h265 --vbitrate 700k -o "%%~F.series.mp4"
+)
+pause
