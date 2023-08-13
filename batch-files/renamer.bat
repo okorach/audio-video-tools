11d0063aa0b3eee7d2086bb949a332c0d29422e3
Olivier Korach <olivier.korach@gmail.com>
2023-08-13 16:32:13 +0200

    Add params

--- batch-files/renamer.bat
+++ batch-files/renamer.bat
@@ -1,10 +1,11 @@
 :: setlocal enabledelayedexpansion
-:: set /p root=Root name ?:
+set /p prefix=Prefix ? 
+set /p seq=Sequence digits ? 
 :: for %%F in (%*) do (
 ::    renamer -f "%%~F" -r "%root%"
 :: )
 
 :: renamer -r "%root%" -f %* -g 5
-set /p seqstart=Sequence Start ?:
-renamer --seqstart %seqstart% --format "Andalousie 2023 - #SEQ4# - #TIMESTAMP# - #DEVICE#" --files %*
+set /p seqstart=Sequence Start ? 
+renamer --seqstart %seqstart% --format "%prefix% - #SEQ%seq%# - #TIMESTAMP# - #DEVICE# - #SIZE# - #FPS#fps - #BITRATE#" -g 5 --files %*
 pause
