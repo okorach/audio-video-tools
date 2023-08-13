daec97034ca03bb96ceb5af5d6471ad39dc0ae6f
Olivier Korach <olivier.korach@gmail.com>
2023-08-13 16:41:26 +0200

    Add format

--- batch-files/renamer.bat
+++ batch-files/renamer.bat
@@ -1,11 +1,10 @@
 :: setlocal enabledelayedexpansion
-set /p prefix=Prefix ? 
-set /p seq=Sequence digits ? 
+set /p format=XXX - #SEQx# - #TIMESTAMP# - #DEVICE# - #SIZE# - #FPS#fps - #BITRATE# ?
 :: for %%F in (%*) do (
 ::    renamer -f "%%~F" -r "%root%"
 :: )
 
 :: renamer -r "%root%" -f %* -g 5
-set /p seqstart=Sequence Start ? 
-renamer --seqstart %seqstart% --format "%prefix% - #SEQ%seq%# - #TIMESTAMP# - #DEVICE# - #SIZE# - #FPS#fps - #BITRATE#" -g 5 --files %*
+set /p seqstart=Sequence Start ?
+renamer --seqstart %seqstart% --format "%format%" --files %*
 pause
