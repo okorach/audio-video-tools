9647be9be994ffd4f5c68e24633bf7a0adf5d1ff
Olivier Korach <olivier.korach@gmail.com>
2023-08-14 20:26:46 +0200

    prefix mode

--- batch-files/renamer.bat
+++ batch-files/renamer.bat
@@ -1,11 +1,12 @@
 :: setlocal enabledelayedexpansion
-set /p pformat=Photo format XXX - #SEQ# - #TIMESTAMP# - #DEVICE# - #SIZE# ?
-set /p vformat=Video format XXX - #SEQ# - #TIMESTAMP# - #DEVICE# - #SIZE# - #FPS#fps - #BITRATE#MB ?
+:: set /p pformat=Photo format XXX - #SEQ# - #TIMESTAMP# - #DEVICE# - #SIZE# ?
+:: set /p vformat=Video format XXX - #SEQ# - #TIMESTAMP# - #DEVICE# - #SIZE# - #FPS#fps - #BITRATE#MB ?
 :: for %%F in (%*) do (
 ::    renamer -f "%%~F" -r "%root%"
 :: )
 
 :: renamer -r "%root%" -f %* -g 5
+set /p prefix=Prefix ?
 set /p seqstart=Sequence Start ?
-renamer --seqstart %seqstart% --video_format "%vformat%" --photo_format "%pformat%" --files %*
+renamer --seqstart %seqstart% --prefix "%prefix%" --files %*
 pause
