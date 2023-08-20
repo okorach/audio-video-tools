333468a928cba1ce7efcc822f370478e40025e4a
Olivier Korach <olivier.korach@gmail.com>
2023-08-20 14:47:31 +0200

    First commit

---
+++ batch-files/datefixer.bat
@@ -0,0 +1,6 @@
+:: setlocal enabledelayedexpansion
+
+set /p offset_plus=offset+ ?
+set /p offset_minus=offset- ?
+datefixer --add_time "%offset_plus%" --remove_time "%offset_minus%" --files %*
+pause
