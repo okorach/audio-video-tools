ef6d69a490c4767dbc26afc4340d6f6d82d3cf7f
Olivier Korach <olivier.korach@gmail.com>
2024-01-27 11:56:47 +0100

    Move impact in .before before scaling

--- batch-files/scale.bat
+++ batch-files/scale.bat
@@ -21,5 +21,7 @@
 setlocal enabledelayedexpansion
 set /p scale=Max size ?:
 for %%F in (%*) do (
-    E:\Tools\ffmpeg\bin\ffmpeg -i "%%~F" -vf scale=%scale%:-1 "%%~F-%scale%.jpg"
+    ren "%%~F" "%%~nxF.before-scale.jpg"
+    E:\Tools\ffmpeg\bin\ffmpeg -i "%%~F.before-scale.jpg" -vf scale=%scale%:-1 "%%~F"
 )
+pause
