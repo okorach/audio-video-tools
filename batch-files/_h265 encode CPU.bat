2423818e81ca24b6f750089ac24e3a41857fce2c
Olivier Korach <olivier.korach@gmail.com>
2024-06-08 11:01:46 +0200

    Add restore of video datestamp

--- batch-files/_h265 encode CPU.bat
+++ batch-files/_h265 encode CPU.bat
@@ -1,7 +1,10 @@
 setlocal enabledelayedexpansion
+
+set /p year=Good year ?
+
 for %%F in (%*) do (
     D:\tools\ffmpeg\bin\ffmpeg -y -i "%%~F" -vcodec libx265 -acodec aac -b:a 128k "%%~F.h265.auto.mp4"
-    datefixer --year=2019 --files "%%~F" "%%~F.h265.auto.mp4"
+    datefixer --year=%year% --files "%%~F" "%%~F.h265.auto.mp4"
 )
 
 ::    D:\tools\ffmpeg\bin\ffmpeg -y -i "%%~F" -vcodec libx265 -an "%%~F.h265.auto.mp4"
