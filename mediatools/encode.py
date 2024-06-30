b046c678d1ca9cc640667b28af6b18bbbc8318b3
Olivier Korach <olivier.korach@gmail.com>
2024-06-30 15:32:16 +0200

    Add keepName

--- mediatools/encode.py
+++ mediatools/encode.py
@@ -24,12 +24,14 @@
 # - Profile specs (from config file)
 # - Original file specs (audio/video codec and bitrate)
 
+import os
 from mediatools import log
 import mediatools.file as fil
 import mediatools.videofile as video
 import mediatools.audiofile as audio
 import mediatools.utilities as util
 import mediatools.options as opt
+import utilities.file as fileutil
 
 def encode_file(file, file_type, **kwargs):
     '''Encodes a single file'''
@@ -37,24 +39,27 @@ def encode_file(file, file_type, **kwargs):
         file_object = audio.AudioFile(file)
     else:
         file_object = video.VideoFile(file)
-        if kwargs.get('width', None) is not None:
-            specs = file_object.get_properties()
-            w, h = int(specs[opt.Option.WIDTH]), int(specs[opt.Option.HEIGHT])
-            new_w = int(kwargs['width'])
-            if kwargs.get('vheight', None) is not None:
-                new_h = int(kwargs.get('vheight', 0))
-            else:
-                new_h = (int(h * new_w / w) // 8) * 8
-            kwargs[opt.Option.RESOLUTION] = f"{new_w}x{new_h}"
+        # if kwargs.get('width', None) is not None:
+        #     specs = file_object.get_properties()
+        #     w, h = int(specs[opt.Option.WIDTH]), int(specs[opt.Option.HEIGHT])
+        #     new_w = int(kwargs['width'])
+        #     if kwargs.get('vheight', None) is not None:
+        #         new_h = int(kwargs.get('vheight', 0))
+        #     else:
+        #         new_h = (int(h * new_w / w) // 8) * 8
+        #     kwargs[opt.Option.RESOLUTION] = f"{new_w}x{new_h}"
 
     if kwargs.get('timeranges', None) is None:
-        file_object.encode(kwargs.get('outputfile', None), **kwargs)
-        return
+        outfile = file_object.encode(kwargs.get('outputfile', None), **kwargs)
+        video.set_creation_date(outfile, video.get_creation_date(file))
+        return outfile
+
 
     ext = util.get_profile_extension(kwargs.get('profile'))
     count = 0
     filelist = []
     timeranges = kwargs.get('timeranges', None).split(',')
+    creation_date = video.get_creation_date(file)
     for t_r in timeranges:
         kwargs[opt.Option.START], kwargs[opt.Option.STOP] = t_r.split('-')
         count += 1
@@ -62,6 +67,8 @@ def encode_file(file, file_type, **kwargs):
         filelist.append(target_file)
         outputfile = file_object.encode(target_file, **kwargs)
         log.logger.info("File %s generated", outputfile)
+        video.set_creation_date(outputfile, creation_date)
+
         print(f"File {outputfile} generated")
 
     if len(timeranges) > 1:
@@ -71,7 +78,9 @@ def encode_file(file, file_type, **kwargs):
             target_file = util.automatic_output_file_name(target_file, file, "combined", ext)
         video.concat(target_file, filelist)
         log.logger.info("Concatenated file %s generated", target_file)
+        video.set_creation_date(target_file, creation_date)
         print(f"Concatenated file {target_file} generated")
+    return target_file
 
 
 def main():
@@ -84,7 +93,13 @@ def main():
     nb_files = len(file_list)
     for i in range(nb_files):
         log.logger.info("%3d/%3d : %3d%% : %s", i + 1, nb_files, (i + 1) * 100 // nb_files, file_list[i])
-        encode_file(file_list[i], fil.get_type(file_list[i]), **kwargs)
+        ofile = encode_file(file_list[i], fil.get_type(file_list[i]), **kwargs)
+        if kwargs.get("keepName", False):
+            splits = file_list[i].split(".")
+            ext = splits.pop()
+            base = ".".join(splits)
+            fileutil.rename(file_list[i], f"{base}.before_encode.{ext}", False)
+            fileutil.rename(ofile, file_list[i])
 
 
 if __name__ == "__main__":
